265f586bd52cf668a304a430aa89b3604adc9a26
unknown <nagarajumuddey@gmail.com>
2023-04-15 23:17:16 +0530

    commit nagaraju first

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+import java.util.logging.Logger;
+
+public class HelloWorld {
+	
+	
+	public static void main(String[] args) {
+		
+		 Logger logger = Logger.getAnonymousLogger();
+		 
+		 logger.info("hello world nagaraju madhavi");
+	}
+
+}
